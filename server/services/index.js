71f7f6082b39c84492e8280e147ce197c2272b9d
Oriol Puig <oriol.puig90@gmail.com>
2017-08-07 17:17:52 +0200

    Player: Create player service

--- server/services/index.js
+++ server/services/index.js
@@ -1,11 +1,13 @@
 import CompetitionService from './competition';
 import LeagueService from './league';
+import PlayerService from './player';
 import SeasonService from './season';
 import TeamService from './team';
 
 export {
   CompetitionService,
   LeagueService,
+  PlayerService,
   SeasonService as default,
   TeamService,
 };
