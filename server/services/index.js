c64c79b7f40b7c411f55bd9dcd3cfe8413856e61
Oriol Puig <oriol.puig90@gmail.com>
2017-08-07 17:13:09 +0200

    Team: Create Team controller

--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,11 @@
 import CompetitionService from './competition';
 import LeagueService from './league';
 import SeasonService from './season';
+import TeamService from './team';
 
 export {
   CompetitionService,
   LeagueService,
   SeasonService as default,
+  TeamService,
 };
